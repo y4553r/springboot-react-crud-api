4ab899ad9f3574a053cd8e53f8eaaac3850c8c24
y4553r <abougueniyasser@outlook.com>
2020-01-17 16:25:01 +0100

    Add datasource configuration to application.properties and create entity package

---
+++ src/main/java/com/y4553r/springboot/demo/mycoolapp/entity/Employee.java
@@ -0,0 +1,19 @@
+package com.y4553r.springboot.demo.mycoolapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	// define fields
+	
+	// define constructors
+	
+	// define getter/setter
+	
+	
+	// define toString
+	
+}
