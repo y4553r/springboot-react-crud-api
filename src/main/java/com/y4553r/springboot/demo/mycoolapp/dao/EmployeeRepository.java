e418c91aa1fce4ff6964b00efba813df5cd667ca
y4553r <abougueniyasser@outlook.com>
2020-01-20 16:02:16 +0100

    Refactor the app to use Spring data rest - remove controller, service, DAO

--- src/main/java/com/y4553r/springboot/demo/mycoolapp/dao/EmployeeRepository.java
+++ src/main/java/com/y4553r/springboot/demo/mycoolapp/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.y4553r.springboot.demo.mycoolapp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.y4553r.springboot.demo.mycoolapp.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
