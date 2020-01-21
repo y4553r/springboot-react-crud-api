3e23d410b46ab3567819edcec4e470410a971125
y4553r <abougueniyasser@outlook.com>
2020-01-21 10:25:53 +0100

    Add the controller and form to create a new employee

--- src/main/java/com/y4553r/springboot/demo/mycoolapp/controller/EmployeeController.java
+++ src/main/java/com/y4553r/springboot/demo/mycoolapp/controller/EmployeeController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.y4553r.springboot.demo.mycoolapp.dao.EmployeeRepository;
 import com.y4553r.springboot.demo.mycoolapp.entity.Employee;
@@ -23,6 +27,33 @@ public class EmployeeController {
 		List<Employee> employees = employeeRepository.findAll();
 		model.addAttribute("employees", employees);
 		return "employeeslist";
+	}
+	
+	@GetMapping("/formForAdd") 
+	public String renderFormForAdd(Model model) {
+		// create a model attribute to bid form data
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "employees/employeeForm";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeRepository.save(employee);
+		return "redirect:/employees/list";
+	}
+	
+	@GetMapping("/formForEdit/{id}")
+	public String renderFormForEdit(Model model, @PathVariable("id") long id) {
+		Employee employee = employeeRepository.findById(id).get();
+		model.addAttribute("employee", employee);
+		return "employees/employeeForm";
+	}
+	
+	@PostMapping("/employees/delete/{id}")
+	public String deleteEmployee(@PathVariable long id) {
+		employeeRepository.deleteById(id);
+		return "redirect:/employees/list";
 	}
 	
 }
